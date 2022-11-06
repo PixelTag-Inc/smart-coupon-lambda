ec913776be343934f49253795e1a0ad8534b66da
Jordan Paul <jordan@getpixeltag.com>
2022-11-05 20:16:08 -0700

    wip update

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 // Used for testing in local env
 require('dotenv').config();
+let nftStore = require('nft.storage');
 
 const Web3 = require('web3');
 const ethers = require('ethers');
 const ERC20FactoryABI = require('./static/SmartCoupon.json');
+const ERC20SourceABI = require('./static/ERC20_Source.json');
 const unlockABI = require('./static/Unlock.json');
+const publicLockABI = require('./static/PublicLock.json');
+
+const nftStorageApiKey = process.env.NFT_STORAGE_API_KEY;
+const nftStorageClient = new nftStore.NFTStorage({ token: nftStorageApiKey })
 
 const factoryAddress = process.env.ERC20_FACTORY_ADDRESS;
 const unlockAddress = process.env.UNLOCK_ADDRESS;
@@ -20,14 +26,20 @@ const apiUserAddress = process.env.API_ETH_ADDRESS;
 let networkId = parseInt(process.env.CHAINID);
 
 const postRequest = async (event) => {
-  if (event.rawPath === '/update-membership-list') {
-    // Add functionality to consume transactions from Alchemy
-    //return upsertSetting(JSON.parse(event.body));
-    return Promise.resolve([]);
-  } else if (event.rawPath === '/reward/erc20') {
+  if (event.rawPath === '/reward/erc20') {
     return await handleCreateERC20Reward(event);
   } else if (event.rawPath === '/reward/erc721') {
     return await handleCreateUnlockReward(event);
+  } else {
+    return Promise.resolve({});
+  }
+}
+
+const putRequest = async (event) => {
+  if (event.rawPath === '/reward/erc20') {
+    return await handleRewardUserERC20(event);
+  } else if (event.rawPath === '/reward/erc721') {
+    return await handleRewardUserERC721(event);
   } else {
     return Promise.resolve({});
   }
@@ -42,25 +54,30 @@ const handler = (event, context) => {
         body: address
       });
     })
-  } 
+  } else if (event.requestContext.http.method === 'PUT') {
+    putRequest(event).then((address) => {
+      console.log('putRequest',address);
+      context.done(null, {
+        statusCode: 200, // default value
+        body: address
+      });
+    })
+  }
 }; 
 
 //function createErc20Token(string calldata tokenName, string calldata symbol, uint256 supply, address to) external returns(address)
 const handleCreateERC20Reward = async (event) => {
-  console.log(event.body, typeof event.body);
   const web3 = new Web3(new Web3.providers.WebsocketProvider(wsUri));
   const apiUserAddressFinal = web3.utils.toChecksumAddress(apiUserAddress);
   web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(apiUserPrivateKey));
   const contract = new web3.eth.Contract(ERC20FactoryABI.abi, factoryAddress);
   //let data = JSON.parse(event.body);
 
-  const tempData = JSON.parse(event.body);
-  console.log(tempData, typeof tempData);
-  console.log(tempData.initialSupply);
-  return contract.methods.createErc20Token(tempData.name, tempData.symbol, tempData.initialSupply, apiUserAddressFinal).estimateGas({from:apiUserAddressFinal}).then((gasAmount) => {
+  const _data = JSON.parse(event.body);
+  return contract.methods.createErc20Token(_data.name, _data.symbol, ethers.utils.parseEther(_data.initialSupply), apiUserAddressFinal).estimateGas({from:apiUserAddressFinal}).then((gasAmount) => {
     const gasprice = gasAmount * 1.4;
     console.log('running post data');
-    return contract.methods.createErc20Token(tempData.name, tempData.symbol, tempData.initialSupply, apiUserAddressFinal).send({from:apiUserAddressFinal, gas: gasprice});
+    return contract.methods.createErc20Token(_data.name, _data.symbol,  ethers.utils.parseEther(_data.initialSupply), apiUserAddressFinal).send({from:apiUserAddressFinal, gas: gasprice});
   }).then((res, err) => {
     console.log('success',res);
     return res.events['0'].address;
@@ -71,22 +88,21 @@ const handleCreateERC20Reward = async (event) => {
   
 }
 
-const handleCreateUnlockReward= async (event) => {
-  console.log(event.body, typeof event.body);
+const handleCreateUnlockReward = async (event) => {
   const web3 = new Web3(new Web3.providers.WebsocketProvider(wsUri));
   const apiUserAddressFinal = web3.utils.toChecksumAddress(apiUserAddress);
   web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(apiUserPrivateKey));
   const contract = new web3.eth.Contract(unlockABI.abi, unlockAddress);
   //let data = JSON.parse(event.body);
 
-  const tempData = JSON.parse(event.body);
+  const _data = JSON.parse(event.body);
   const salt = web3.utils.randomHex(12);
   return contract.methods.createLock(
     ethers.constants.MaxUint256, 
     ethers.constants.AddressZero, 
     ethers.constants.Zero, 
     ethers.constants.MaxUint256, 
-    tempData.name, 
+    _data.name, 
     salt
   ).estimateGas({from:apiUserAddressFinal})
   .then((gasAmount) => {
@@ -98,12 +114,77 @@ const handleCreateUnlockReward= async (event) => {
       ethers.constants.AddressZero, 
       ethers.constants.Zero, 
       ethers.constants.MaxUint256, 
-      tempData.name, 
+      _data.name, 
       salt
     ).send({from:apiUserAddressFinal, gas: ethers.BigNumber.from(gasprice.toFixed(0))});
   }).then((res, err) => {
     console.log('success',res, err);
+    
     return res.events['NewLock'].address;
+  }).catch((err) => {
+    console.log('error',err);
+    return err;
+  })
+}
+
+const handleRewardUserERC20 = async (event) => {
+  const _data = JSON.parse(event.body); // contractAddress, amount, walletAddress
+  const web3 = new Web3(new Web3.providers.WebsocketProvider(wsUri));
+  const apiUserAddressFinal = web3.utils.toChecksumAddress(apiUserAddress);
+  web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(apiUserPrivateKey));
+  const contract = new web3.eth.Contract(ERC20SourceABI.abi, web3.utils.toChecksumAddress(_data.contractAddress));
+  //let data = JSON.parse(event.body);
+
+  return contract.methods.mint(web3.utils.toChecksumAddress(_data.walletAddress),  ethers.utils.parseEther(_data.amount)).estimateGas({from:apiUserAddressFinal}).then((gasAmount) => {
+    const gasprice = gasAmount * 1.4;
+    console.log('running post data');
+    return contract.methods.mint(web3.utils.toChecksumAddress(_data.walletAddress), ethers.utils.parseEther(_data.amount)).send({from:apiUserAddressFinal, gas: gasprice});
+  }).then((res, err) => {
+    console.log('success',res);
+    return 'success';
+  }).catch((err) => {
+    console.log('error',err);
+    return err;
+  })
+  
+}
+
+const handleRewardUserERC721 = async (event) => {
+  const _data = JSON.parse(event.body); // contractAddress, amount, walletAddress, name, imageUrl, symbol
+  const web3 = new Web3(new Web3.providers.WebsocketProvider(wsUri));
+  const apiUserAddressFinal = web3.utils.toChecksumAddress(apiUserAddress);
+  web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(apiUserPrivateKey));
+  const contract = new web3.eth.Contract(publicLockABI.abi, web3.utils.toChecksumAddress(_data.contractAddress));
+  //let data = JSON.parse(event.body);
+
+  const salt = web3.utils.randomHex(12);
+  const nft = await nftStorageClient.store({
+    name: _data.name,
+    description: _data.name+' NFT',
+    image: _data.imageUrl
+  });
+
+  
+  return contract.methods.grantKeys(
+    [web3.utils.toChecksumAddress(_data.walletAddress)], 
+    [ethers.constants.MaxUint256],
+    [apiUserAddressFinal]
+  ).estimateGas({from:apiUserAddressFinal})
+  .then((gasAmount) => {
+    console.log(gasAmount);
+    const gasprice = gasAmount * 1.4;
+    console.log('running post data');
+    return contract.methods.grantKeys(
+      [web3.utils.toChecksumAddress(_data.walletAddress)], 
+      [ethers.constants.MaxUint256],
+      [apiUserAddressFinal]
+    ).send({from:apiUserAddressFinal, gas: ethers.BigNumber.from(gasprice.toFixed(0))});
+  }).then((res, err) => {
+    console.log('success grantKeys',res, err);
+    return contract.methods.setLockMetadata(_data.name, _data.symbol, nft.url);
+  }).then((res, err) => {
+    console.log('success setLockMetadata',res, err);
+    return 'success';
   }).catch((err) => {
     console.log('error',err);
     return err;
@@ -120,7 +201,7 @@ web3.eth.accounts.wallet.add(web3.eth.accounts.privateKeyToAccount(apiUserPrivat
 const contract = new web3.eth.Contract(unlockABI.abi, unlockAddress);
 //let data = JSON.parse(event.body);
 
-const tempData = {
+const _data = {
   "type": "erc721",
   "symbol": "AD1-A",
   "name": "Welcome NFT",
@@ -133,7 +214,7 @@ return contract.methods.createLock(
   ethers.constants.AddressZero, 
   ethers.constants.Zero, 
   ethers.constants.MaxUint256, 
-  tempData.name, 
+  _data.name, 
   salt
 ).estimateGas({from:apiUserAddressFinal})
 .then((gasAmount) => {
@@ -145,7 +226,7 @@ return contract.methods.createLock(
     ethers.constants.AddressZero, 
     ethers.constants.Zero, 
     ethers.constants.MaxUint256, 
-    tempData.name, 
+    _data.name, 
     salt
   ).send({from:apiUserAddressFinal, gas: ethers.BigNumber.from(gasprice.toFixed(0))});
 }).then((res, err) => {
